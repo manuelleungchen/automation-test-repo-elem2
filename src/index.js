4eafdd7fadb06f54d91f116d726e2c5148700c38
Manuel Leung Chen <manuel.leungchen@gmail.com>
2023-05-11 13:32:19 -0400

    Updating test repo files

---
+++ src/index.js
@@ -0,0 +1,19 @@
+var $ = require('jquery');
+window.jQuery = $;
+window.$ = $;
+
+require('bootstrap');
+require('k8-bootstrap-css');
+require('html5-media-controller');
+require('k8-components');
+require('k8-top-nav');
+require('@digital-learning/iframe-resizer');
+
+// require('@digital-learning/ebook-reader');
+require('@digital-learning/fill-in-the-blank');
+// require('@digital-learning/hundred-chart');
+require('@digital-learning/matching');
+// require('@digital-learning/money');
+// require('@digital-learning/multiple-choice');
+// require('@digital-learning/number-line');
+// require('@digital-learning/ten-frame');
